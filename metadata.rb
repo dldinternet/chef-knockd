084782596361742e0b741f5f857dc6dd0ed47ede
Christo DeLange <cdelange@amplify.com>
2014-06-04 21:45:39 -0400

    Name attribute

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name             "knockd"
 maintainer       "Igor Afonov"
 maintainer_email "afonov@gmail.com"
 license          "MIT"
 description      "Installs/Configures knockd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
