1f79252bac922d25f48d971bbecb6b2443e5d72e
Christo DeLange <cdelange@amplify.com>
2014-06-05 06:15:01 -0400

    State what we support

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,7 @@ maintainer_email "afonov@gmail.com"
 license          "MIT"
 description      "Installs/Configures knockd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.4"
+version          "0.0.5"
+
+supports 'ubuntu'
+supports 'debian'
